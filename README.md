c00f8591e548e00bba0a1200522027733149d66d
sabiquekhan <sabiqueislamkhan@gmail.com>
2022-08-02 22:57:11 +0600

    Environment Setup Done

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Image-Matching-Python-Script
+
+# First I have install Selenium in Ubuntu:
+#   - sudo install selenium
+
+# Then I have installed BeautifulSoup:
+#   - sudo apt-get install python3-bs4
+
+# Then I have followed a document for installing the webdriver:
+#   - https://tecadmin.net/setup-selenium-chromedriver-on-ubuntu/
---
+++ python_web_scrapping.py
@@ -0,0 +1,51 @@
+# -----BEGIN OPENSSH PRIVATE KEY-----
+# b3BlbnNzaC1rZXktdjEAAAAACmFlczI1Ni1jdHIAAAAGYmNyeXB0AAAAGAAAABAYycbLly
+# GTHrK9UuAMd9hLAAAAEAAAAAEAAAGXAAAAB3NzaC1yc2EAAAADAQABAAABgQDPc6SVQCkL
+# AgG9GPjY//JNI9mXAbj8EafIspxk8Pwepxv1Wj4i8nFgTqPN0m6P88UYUShqusBbwbK56A
+# fnOs3IKOFzDpPOJ+kNAyIXaLr4m2aDvbjwid1WZ1HW4ituk1mmT/mMGyITOw1fxJ+wI/BN
+# ehsuTTe3afq1hNzRNrsEgg/rOnWws8esxeFz5VpH3ToO/hCwGDgcwRimrLNHpNZHEeKVtn
+# cIaNRhBZXs4nd3gTY6ilZEORkZGiBs8HIppOzonjSnS61T1gEaVRa71vzaNSJr2I/MXwy7
+# ju0iNqoz3P7uSP8W4A5yZgX1t8Qp3pZnv6FaQFV+U4q3DVlOZxz0HzoI1Hqx8BxE0+kTom
+# VioO+UqRzKlRLQmSh2CzmtYDoLqlgdar51u+xZ4AefD7ZUBhhMvk34WhuatJw6VovKpPm9
+# FQsTWxzzCVHEXuNk2YeHu7YzT1on4avyGfT770Rtkp9U4hb0I+DeMptiuL73Q9vDvi+zz1
+# 48a+PKc+khi1EAAAWQ55ePYx0A354EHFABYgOKNN//nM+VUfpsL+b4l1RICDHNhZA8lUR6
+# mS712HQHgbT+k/U7OkMZ0DmcrO/BvBCQ93Qk0LZ+s7PYkf+s/2CltMtzjO/MLxg10auiPH
+# qsINgBqd1Bxe9D26+bbYGnQByUPDbjiuUA8yKRn5OqJvgwc2VGA7b+W7ZviBUKMsfU5L39
+# 8HMJXieZUeF4qs750/f6AWDqhgY1VpQXUu02Gyz6MVJSAkCZ2CcLnEfOp+e30vJHdMI0N5
+# +rB9NGBNyD1XF+l/tWMseZvj+vo2Gt2GB69W5BJqoPIgyw86GVNIf2Y8oM6i5RA/oVVged
+# fcnWr9NIx5cZn8Wp1wi5yvf2mWJs6Cs43vNXalDnGEaq8xrK6H+2jx6nHZQIzW4i9GxgRI
+# /2pBvQhtY8DwY11hD2SSuTP5RyfeMsdcFd2TVfSgVJta359T5k0/5uB0v5o7XnlOcPHWwB
+# qcvZSabTnLekTYg47HjVk5v39Ypp2EnpB0Joj5tw587gH35eJsRYA9RB7GzAovrrNriAA4
+# ci2np7NkGtSgZe6afzjSCx+0O9+fri8z8w7WJalL9YKRlZcZo7UOFF/+wbdU4HWwVRbzlD
+# vW2Lbl9oVACnx2V9zBSfb08doF3+Iy9As2vgDRjpm7IxBHNyxhlsrZ6ro9yzIO3hEe9lgB
+# C0OQA0HpweCl9BWMSQ3Q1mB/GeLPtZKSypx/FCpcj/QOrsfuUKvITmgd2vuStXnc0Rf1Ww
+# r9WN4wAJxnZznnwc81eeMCz5UmFH3wN40ZvQ//LskNzI5SDb1MPFOWkqlaQ7zPj7vHtEeh
+# s9i85doFBmxrmP/1izdNWx7AKOAMT0bx/eBNSS2I2Maex2I2U+oQrVQpvB5WgkwH6PPftR
+# 2PFrnYIrI/gzAUV+nE9mzZXi3cnvV2NqVZv1XyRrjjPLP4LUjmizPA2auuSpt5cTPVe6Fz
+# 6bE/s5/QPp55PIh5ZkmAkdELiKp44dv6Unp1cSlL9IWFI/7aufTjvimBrfu4R/QeiQKJMb
+# +SbsBfDScUFrn3MwMKJOtEekv5F1YJgOXDRfA77pFBQugAaBWDRMyKw37sIbGM5ZAEZvBd
+# XutJDr8rZb+QcmmGKb0Hrs4M0tiQw08asBt5pYt895kdG/WZVvjrsoAq9oIb3T3g/lzB3R
+# J14EObS6kzQDEw0+o9b5Z+OJRaNnD4+rzO8Iafs0bmrOXlIYCEDoPdmdiFBMIrDEBB+00d
+# gLCcbrc69RQ83nDnQBv13jN6StM8BL3bVKxEbOG0v1DctIUkZSRkdT/mD4cBTBdreTw8Vk
+# pdTCz235o57swZmad2N80SuOP3u/m0vkGwRqglkQ+YyIz25vdM70tcqGNVwD04QhfyZxgC
+# I5f0ZUyl1sCX18WeCiDKfCmm8jbbKwG+y+McbGlN6ixggJ76BqaLsxsWnmJe3UbZ96U6zi
+# HyXI6pshS23Cc94IzScXFR3KBmWARnI7myQcl02XFDUdsl7cvcdCLTN9YkMKBuLLAAq3EQ
+# cs7KAWF0+EvyyStc7MV4nU1OlIvwIHncBNAv+H4IsK1SbMeOJqXptQU09ertOIMlMvs7Gb
+# IMIXw5D32M+dcQkmB1sOAFQ91sdFLwb/3REE2EUx8hKOe3TBFwpg0iYfa1XLcAszgLZhK4
+# zYxcF9Kn2KYvtyWsgY95sBKM6/bYKG2Yle9dVg5xH80YYLM70KdRIOivfDasbyByTyIJSC
+# UXbBn/fo5QWv0zhjE+JmxcKQYhxaGWakddFCG94Qwal8A++JwoNyydYrFYVoqim7tQPiCZ
+# jvDTHD7yXls16YhIHkXApLl0mDc2ScwnFh3TodULjYD1bipH6Y4UVh7PL3wtADlo81xYqA
+# qdlfq18TF++0hvbGyRVgmFpvbsk=
+# -----END OPENSSH PRIVATE KEY-----
+
+from selenium import webdriver
+import bs4
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+import requests
+
+driver = webdriver.Chrome("/home/asus/chromedriver")
+
+search_url = "https://www.freepik.com/"
+driver.get(search_url)
